88ba1fd6937fa37321e07d0252d93ffdf486d110
derkells <ellsworth.derek90@gmail.com>
2022-10-11 20:14:26 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,31 @@
-const submitToServer = function() {
-    console.log("hello")
-
+//Reset button. Resets the output
+function resetOutput() {
+    document.getElementById("output").innerHTML = ""
 }
 
+//Calls API and returns the json package for Random Activity
+const getRandomActivity = async () => {
+    let response = String
+    const APIResponse = await fetch('http://www.boredapi.com/api/activity/');
+    const givenResponse = await APIResponse.json();
+    response = givenResponse.activity
 
-function myFun() {
-    document.getElementById("output").innerHTML = "Yeet"
+    document.getElementById("output").innerHTML = response
 }
+//Calls API and returns json package for the application
+const getSpecificActivity = async () => {
+    let response1 = String
+    let APIAddress = `http://www.boredapi.com/api/activity?participants=${sliderTest()}`
+    const APIResponse1 = await fetch(APIAddress)
+    const givenResponse1 = await APIResponse1.json()
+    response1 = givenResponse1.activity
+
+    document.getElementById("output").innerHTML = response1
+}
+
+function sliderTest() {
+    var test1 = parseInt(document.getElementById("firstSlider").value)
+    console.log(test1)
+    return test1
+}
+
